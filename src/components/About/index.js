1bb04c04c29829cff0a36907ac7771e9c4e196f1
dave vebber <davevebber@gmail.com>
2021-05-10 18:57:41 -0700

    inital files

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Who am I?</h1>
+      <p>  
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium facilis, accusantium quod modi, id dolores rerum sint suscipit repellat deserunt quas, aliquam similique sequi. Est eius similique explicabo quo aliquam.
+      </p>
+    </section>
+  );
+}
+
+export default About;
