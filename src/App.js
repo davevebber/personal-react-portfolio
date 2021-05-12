ce7e6a5af9edcf5355f79ebc0f296052ec9faae7
dave vebber <davevebber@gmail.com>
2021-05-11 18:21:55 -0700

    contact form now appears. fixed issue with pictures being too big

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact'
 
 function App() {
   const [categories] = useState([
@@ -13,16 +14,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <Portfolio currentCategory={currentCategory}></Portfolio>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
